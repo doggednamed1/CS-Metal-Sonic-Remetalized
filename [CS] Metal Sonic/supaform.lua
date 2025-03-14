b977af45d18226dd47a9c139b7fdf49348e58d64
doggednamed1 <59123316+doggednamed1@users.noreply.github.com>
2025-03-14 14:37:20 -0600

    updated supaform.lua

--- [CS] Metal Sonic/supaform.lua
+++ [CS] Metal Sonic/supaform.lua
@@ -3,6 +3,7 @@ local GLIMMERING_GIFT = audio_stream_load("super_theme.mp3")
 local ULTIMATE_FORCE = audio_stream_load("hyper_theme.mp3")
 
 local huehuehuehyper = audio_stream_load("maniaspeedway.mp3")
+local mechahyper = audio_stream_load("Doomsday.ogg")
 function supertheme()
     _G.SuperThemeExtra = true
     if _G.charSelect.character_get_current_number() == METAL_SONIC then
@@ -17,7 +18,19 @@ function supertheme()
                 silencetheme = silencetheme - 10
             end
         end
-    else
+    elseif _G.charSelect.character_get_current_number() == MECHA_SONIC_MK2 then
+        if _G.IsSuper == true or _G.IsHyper == true then
+            audio_stream_play(mechahyper, false, 1)
+            play_cap_music(0)
+            sliencetheme = 100
+        else
+            audio_stream_stop(mechahyper)
+            if silencetheme > 0 then
+                stop_cap_music(0)
+                silencetheme = silencetheme - 10
+            end
+        end
+    elseif charSelect.character_get_current_number ~= METAL_SONIC or MECHA_SONIC_MK2 then
         if _G.IsSuper == true then
             audio_stream_play(GLIMMERING_GIFT, false, 1)
             play_cap_music(0)
